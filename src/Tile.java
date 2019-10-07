1df12476d970781155a4ac49982a7852677b5f76
paragk05 <paragk106@gmail.com>
2019-10-07 16:37:00 +0530

    Tile, Co-ordinate, Tile Occupied or Not

---
+++ src/Tile.java
@@ -0,0 +1,60 @@
+public abstract class Tile
+{
+
+    int tileCoordinate;
+
+    Tile(int tileCoordinate)
+    {
+        this.tileCoordinate=tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+
+    //SubClass that represents the tile is occupied or Empty
+
+    public static final class EmptyTile extends Tile
+    {
+        EmptyTile(int coordinate)
+        {
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied()
+        {
+            return false;
+        }
+        @Override
+        public Piece getPiece()
+        {
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile
+    {
+        Piece pieceOnTile;
+
+        OccupiedTile(int tileCoordinate,Piece pieceOnTile)
+        {
+            super(tileCoordinate);
+
+            this.pieceOnTile=pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied()
+        {
+            return true;
+        }
+
+        @Override
+        public  Piece getPiece()
+        {
+            return this.pieceOnTile;
+        }
+    }
+}
